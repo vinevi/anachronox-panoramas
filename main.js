65d8201bdf277fd17b580446c77be274391ee54c
encore-by-dev <dev@encore.by>
2021-01-03 14:39:18 +0300

    stereo

---
+++ main.js
@@ -0,0 +1,25 @@
+(function(){
+	let viewer = new PANOLENS.Viewer({
+		autoHideControlBar: true,
+		enableReticle: false,
+	});
+	let stereo = new PANOLENS.Stereo( 0.02 );
+	viewer.sceneReticle.visible = false;
+	document.addEventListener('DOMContentLoaded', function(){
+		let selectElement = document.querySelector('#cubemap-select');
+		let options = selectElement.querySelectorAll('option');
+		let panoramas = [];
+		options.forEach(function(option){
+			let path = option.dataset.path;
+			console.log(path);
+			let panorama = new PANOLENS.StereoImagePanorama(path, stereo);
+			viewer.add(panorama);
+			panoramas.push(panorama);
+		});
+		selectElement.addEventListener('change', function(event){
+			let index = event.target.selectedIndex;
+			viewer.setPanorama(panoramas[index]);
+			viewer.disableReticleControl();
+		});
+	});
+})();
