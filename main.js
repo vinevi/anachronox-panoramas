38027ca69b422097e03a8b019e88ccc79280d773
encore-by-dev <dev@encore.by>
2021-01-04 23:08:10 +0300

    rowdys

--- main.js
+++ main.js
@@ -1,25 +1,73 @@
-(function(){
 	let viewer = new PANOLENS.Viewer({
 		autoHideControlBar: true,
 		enableReticle: false,
 	});
-	let stereo = new PANOLENS.Stereo(0.04);
+
+	let stereo = new PANOLENS.Stereo(0);
+
+	let panoramas = [
+		{
+			"name": "Whitendon",
+			"cubemap": "panoramas/whitendon/01/cubemap/",
+			"stereo": "panoramas/whitendon/01/whitendon_01.png"
+		},
+		{
+			"name": "Rowdys - Boot's office",
+			"cubemap": "panoramas/rowdys/01/cubemap/",
+		}
+	];
+
+	
+	let currentPanorama;
 	viewer.sceneReticle.visible = false;
 	document.addEventListener('DOMContentLoaded', function(){
-		let selectElement = document.querySelector('#cubemap-select');
-		let options = selectElement.querySelectorAll('option');
-		let panoramas = [];
-		options.forEach(function(option){
-			let path = option.dataset.path;
-			console.log(path);
-			let panorama = new PANOLENS.StereoImagePanorama(path, stereo);
-			viewer.add(panorama);
-			panoramas.push(panorama);
-		});
-		selectElement.addEventListener('change', function(event){
-			let index = event.target.selectedIndex;
-			viewer.setPanorama(panoramas[index]);
-			viewer.disableReticleControl();
-		});
+		let panoramaControls = document.querySelector('#panorama-controls');
+		let selectElement = panoramaControls.querySelector('#cubemap-select');
+		let stereoCheckbox = panoramaControls.querySelector('input#stereo');
+		panoramas.forEach(function(panorama) {
+			let optionElement = document.createElement('option');
+			let name = panorama.name;
+			optionElement.innerHTML = name;
+			selectElement.appendChild(optionElement);
+		})
+
+		function loadCurrentPanorama() {
+			let isStereo = stereoCheckbox.checked;
+			let panoramaIndex = selectElement.selectedIndex;
+			let panoramaInfo = panoramas[panoramaIndex];
+			if (isStereo) {
+				let stereoPath = panoramaInfo.stereo;
+				if(stereoPath) {
+					if(currentPanorama) {
+						viewer.remove(currentPanorama);
+					}
+					currentPanorama = new PANOLENS.StereoImagePanorama(stereoPath, stereo);
+					viewer.add(currentPanorama);
+					viewer.setPanorama(currentPanorama);
+				}
+			} else {
+				let cubemapPath = panoramaInfo.cubemap;
+				if(cubemapPath) {
+					let format = '.png';
+					if(currentPanorama) {
+						viewer.remove(currentPanorama);
+					}
+					currentPanorama = new PANOLENS.CubePanorama([
+						cubemapPath + 'posx' + format,
+						cubemapPath + 'negx' + format,
+						cubemapPath + 'posy' + format,
+						cubemapPath + 'negy' + format,
+						cubemapPath + 'posz' + format,
+						cubemapPath + 'negz' + format
+					]);
+					viewer.add(currentPanorama);
+					viewer.setPanorama(currentPanorama);
+				}
+			}
+		}
+
+		selectElement.addEventListener('change', loadCurrentPanorama);
+		stereoCheckbox.addEventListener('change', loadCurrentPanorama);
+
+		loadCurrentPanorama();
 	});
-})();
